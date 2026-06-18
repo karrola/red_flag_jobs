def02481f7702564c8ba48ccc697d1ba0a1646b6
karrola <karolinahadzinska@gmail.com>
2026-06-18 14:08:53 +0200

    return to local deploy

--- red_flag_jobs/settings.py
+++ red_flag_jobs/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qu2uz=kx#z)lbo!-@m1hh42c=faf!@=qtavw)u60nu1o_c#vl@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["redflagjobs.onrender.com"]
+ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ["redflagjobs.onrender.com"]
 
 # Application definition
 
@@ -42,7 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    # "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -53,8 +54,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'red_flag_jobs.urls'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 TEMPLATES = [
     {
